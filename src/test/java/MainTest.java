a76e655dce979759c57fd521973f673db03bcb17
Kosenko <Kosenko.2000@yandex.ru>
2022-12-04 20:15:19 +0500

    add new project

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,14 @@
+import org.junit.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void test_sum(){
+        int sum = 4;
+        Assertions.assertEquals(4,sum);
+    }
+
+
+}
